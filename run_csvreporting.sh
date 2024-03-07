afca183992699cfce42bd387c87684a67f2c4e5f
Ang3lBlack1 <160098159+Ang3lBlack1@users.noreply.github.com>
2024-03-07 13:29:41 -0600

    Update run_csvreporting.sh

--- run_csvreporting.sh
+++ run_csvreporting.sh
@@ -2,10 +2,6 @@
 #DIA=`date +"%d/%m/%Y"`
 #HORA=`date +"%H:%M"`
 
-#APP_DIR=$(dirname $0)
-#source ${APP_DIR}/venv/bin/activate
-#python3 ${APP_DIR}/app_csvreporting.py $1 >> ${APP_DIR}/csvreporting.log
-#deactivate
 APP_DIR=$(dirname "$0")
 source "${APP_DIR}/venv/bin/activate"
 
