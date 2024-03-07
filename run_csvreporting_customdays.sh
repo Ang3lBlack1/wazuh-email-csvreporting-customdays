629e2a8e710449da4bb69f2bb0a8a72076bc040e
Ang3lBlack1 <160098159+Ang3lBlack1@users.noreply.github.com>
2024-03-07 14:26:52 -0600

    Update run_csvreporting_customdays.sh

--- run_csvreporting_customdays.sh
+++ run_csvreporting_customdays.sh
@@ -9,7 +9,7 @@ source "${APP_DIR}/venv/bin/activate"
 current_date=$(date +"%Y-%m-%d %T")
 
 # Ejecuta el script de Python y redirige la salida al archivo csvreporting.log con la fecha y hora
-python3 "${APP_DIR}/app_csvreporting.py" "$1" "$2" "$3" | while IFS= read -r line; do
+python3 "${APP_DIR}/app_csvreporting_customdays.py" "$1" "$2" "$3" | while IFS= read -r line; do
     echo "[$current_date] $line" >> "${APP_DIR}/csvreporting.log"
 done
 
